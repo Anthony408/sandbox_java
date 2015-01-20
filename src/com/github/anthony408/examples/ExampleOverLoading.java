86a79d6050640350ada358ac7f2ea816de3df0f6
agaglian <agaglian@cisco.com>
2015-01-19 23:42:46 -0800

    +1

--- src/com/github/anthony408/examples/ExampleOverLoading.java
+++ src/com/github/anthony408/examples/ExampleOverLoading.java
@@ -1,7 +1,41 @@
 package com.github.anthony408.examples;
 
-/**
- * Created by agaglian on 1/19/15.
- */
+
+import java.util.Random;
+
+
 public class ExampleOverLoading {
+
+    private int getRandomNumber(){
+        Random r = new Random();
+        return r.nextInt(); 
+    }
+    
+    private int getRandomNumber(int seed){
+        Random r = new Random(seed);
+        return r.nextInt();
+    }
+
+    private int getRandomNumber(int seed, int range){
+        Random r = new Random(seed);
+        return r.nextInt(range) + 1; // so range goes from 1-->range.
+    }
+
+
+
+    public static void main(String[] args) {
+        ExampleOverLoading me = new ExampleOverLoading();
+        System.out.println("Signature's 1 use:"+ me.getRandomNumber());
+        System.out.println("Signature's 2 use:"+ me.getRandomNumber(1));
+        System.out.println("Signature's 3 use:"+ me.getRandomNumber(123,1000));
+    }
+    
 }
+
+/*
+OUTPUT
+------
+Signature's 1 use:332908814
+Signature's 2 use:-1155869325
+Signature's 3 use:783
+ */
